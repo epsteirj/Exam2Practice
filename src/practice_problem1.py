4f8cbb52801b49dda6e7a159517552c18d41bc28
epsteirj <epsteirj@rose-hulman.edu>
2018-01-15 15:37:23 -0500

    aaaaaaaa

--- src/practice_problem1.py
+++ src/practice_problem1.py
@@ -476,7 +476,7 @@ class Box(object):
           :type other_box: Box
         """
         # --------------------------------------------------------------
-        # TODO: 10. Implement and test this function.
+        # Done: 10. Implement and test this function.
         #     The testing code is already written for you (above).
         # --------------------------------------------------------------
         # --------------------------------------------------------------
